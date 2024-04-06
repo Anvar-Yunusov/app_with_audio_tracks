d01f5fa221212ca390ab29fc9101045630589488
Anvar-Yunusov <anwar-qa-automation@bk.ru>
2024-04-06 11:15:57 +0300

    дизайн приложения

---
+++ lib/screens/app_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AudioAppScreen extends StatefulWidget {
+
+  @override
+  State<AudioAppScreen> createState() => _AudioAppScreenState();
+}
+
+class _AudioAppScreenState extends State<AudioAppScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
