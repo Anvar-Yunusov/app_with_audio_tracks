06204f37a80fd27e4a8f0ea9d88dbc1ec1c3f80a
Anvar-Yunusov <anwar-qa-automation@bk.ru>
2024-04-06 13:27:44 +0300

    Добаление фото и аудио на страницу приложения

---
+++ lib/models/item_model.dart
@@ -0,0 +1,13 @@
+
+
+class Item{
+
+  final String name;
+  final String ImagePath;
+
+  final String audioPath;
+
+
+  Item({required this.audioPath, required this.ImagePath, required this.name});
+
+}
